67d2ace400f18ea3f6d1659b7aa4899156dd745f
xyzeebit <gotodonald@gmail.com>
2023-10-12 08:27:29 +0100

    Add run function
    run() opens and reads file
    Add Result struct to hold search data

--- minigrep.cpp
+++ minigrep.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+
+struct Result
+{
+    int line;
+    int position;
+    std::string text;
+};
 
 struct Minigrep
 {
-    int line_number;
     std::string word;
     std::string path;
-    std::string line;
+    std::vector<Result> lines;
+    std::string text;
 
     Minigrep(std::string word_to_find, std::string filepath) : path{filepath}, word{word_to_find}
     {}
+
+    void search(const std::string w, const std::string t)
+    {
+    }
+
+    void run()
+    {
+        std::ifstream file(path, std::ios_base::in);
+        if(file.is_open())
+        {
+            while(file.good())
+            {
+                std::getline(file, text);
+                search(word, text);
+            }
+            file.close();
+        }
+    }
 };
 
 int main(int argc, char* argv[])
 {
     if(argc >= 2)
     {
-        std::cout << "Word: " << argv[1]
-            << " File path: " << argv[2]
-            << std::endl;
+        Minigrep mg {argv[1], argv[2]};
+        mg.run();
     }
     else
     {
         std::cout << "Not enough arguments pass into program" << std::endl;
-        std::cout << "Usage" << std::endl;
+        std::cout << "Usage:" << std::endl;
         std::cout << "./minigrep hello helloword.txt" << std::endl;
     }
 }
